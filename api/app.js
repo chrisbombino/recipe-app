108acd6d313d48dcb87eff325c2d13176e088b7c
chrisbombino <32116781+chrisbombino@users.noreply.github.com>
2020-03-31 17:49:54 -0400

    Get coming-soon image from S3 Bucket
    I decided to store the iamges for this project in AWS S3 as a way to
    learn more about the AWS ecosystem.
    
    A GET request to /api/images/:key gets the signed url from S3
    associated with that key and returns it to the client. If that key
    doesn't exist the API returns 404 and if there is some other error
    with getting the signed url, the API returns 500.
    
    On the client side, for each recipe returned from DynamoDB, a request is
    made to the API for the signed url.
    TODO: Add the S3 image key to DynamoDB and use provide that to the API
    to get the recipe specific image.

--- api/app.js
+++ api/app.js
@@ -5,9 +5,32 @@ const auth = require('./.aws-auth'); // ./aws-auth.js is hidden from source cont
 
 AWS.config.update(auth);
 
+// AWS objects
+const s3 = new AWS.S3();
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-app.get('/api/recipes', (req, res) => {
+
+// Middleware
+let imageVerifier = (req, res, next) => {
+  var params = {
+    Bucket: 'chris-recipe-app',
+    Key: `${req.params.key}.jpg`,
+  };
+
+  s3.headObject(params, (err, data) => {
+    if(err) {
+      res.status(404);
+      next('err')
+      return;
+    } else {
+      req.s3params = params;
+      next();
+    }
+  })
+}
+
+// GET all recipes
+app.get('/api/v1/recipes', (req, res) => {
   const params = {TableName: 'Recipes'};
 
   docClient.scan(params, (err, data) => {
@@ -18,6 +41,15 @@ app.get('/api/recipes', (req, res) => {
       res.send(aws_recipes);
     }
   });
+});
+
+// GET signed url for an image
+// TODO implement caching mechanism to keep urls for 1hr
+app.get('/api/v1/images/:key', imageVerifier, (req, res) => {
+    s3.getSignedUrl('getObject', req.s3params, (err, url) => {
+      // return 500 if err, otherwise 200
+      return err ? res.sendStatus(500) : res.status(200).send(url);
+    });
 });
 
 const port = process.env.PORT || 5000
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
@@ -9,7 +8,40 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import Link from '@material-ui/core/Link';
 
 class RecipeBox extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: true,
+      error: false,
+      img_url: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('/api/v1/images/coming-soon')
+      .then(res => {
+        if(res.status !== 200) {
+          this.setState({
+            loading: false,
+            error: true
+          });
+        } else return res.text();
+      })
+      .then(data => {
+        this.setState({
+          loading: false,
+          img_url: data
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    if(this.state.loading) {
+      return(<h1>Loading</h1>)
+    } else if(this.state.error) {
+      return(<h1>Error</h1>)
+    } else {
     return (
       <Grid item xs={6} md={4} xl={3}>
         <Card>
@@ -17,7 +49,7 @@ class RecipeBox extends React.Component {
           <Link target={'_blank'} href={this.props.url}>
             <CardMedia
               component="img"
-              image="https://upload.wikimedia.org/wikipedia/commons/1/13/Spaghetti_carbonara_-_Trishhhh.jpg"
+              image={this.state.img_url}
               height="150"
             />
             <CardContent>{this.props.name}</CardContent>
@@ -25,21 +57,27 @@ class RecipeBox extends React.Component {
         </CardActionArea>
         </Card>
       </Grid>
-    );
+    );}
   }
 }
 
 class RecipeList extends React.Component {
   constructor(props){
     super(props);
-    this.state = {recipes: []}
+    this.state = {
+      loading: true,
+      recipes: []
+    }
   }
 
   componentDidMount(){
-    fetch('/api/recipes')
+    fetch('/api/v1/recipes')
       .then(res => res.json())
       .then(json => {
-        this.setState({recipes: json})
+        this.setState({
+          loading: false,
+          recipes: json
+        })
       })
       .catch(err => (console.log(err)));
   }
@@ -47,11 +85,17 @@ class RecipeList extends React.Component {
     const recipes = this.state.recipes.map((r) =>
       <RecipeBox key={r.name} name={r.name} url={r.url}/>
     );
-    return (
-      <Grid style={{margin: '0px', width:'100%', padding: '0px 150px'}} container spacing={7}>
-        {recipes}
-      </Grid>
-    );
+
+    if(this.state.loading) {
+      return(<h1>LOADING</h1>);
+    } else {
+      return (
+          <Grid style={{margin: '0px', width:'100%', padding: '0px 150px'}} container spacing={7}>
+            {recipes}
+          </Grid>
+      );
+    }
+
   }
 }
 
