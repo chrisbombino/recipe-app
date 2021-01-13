9b8fb98064dd4292cb4dac67da9e27181ed7d62b
chrisbombino <cjbombino@gmail.com>
2021-01-12 20:06:25 -0500

    Add Tests for New API Routes
    Add unit tests for the new API routes that were added. Some of these
    tests (the ones that need to call DynamoDB) need some more work to
    mock the DB calls.

--- api/test/recipes.js
+++ api/test/recipes.js
@@ -5,7 +5,7 @@ let expect = chai.expect
 
 chai.use(chaiHttp);
 
-describe('GET api/v1/recipes', () => {
+describe('GET /api/v1/recipes', () => {
   it('Should return all recipes', (done) => {
     chai.request(app)
       .get('/api/v1/recipes')
@@ -18,6 +18,45 @@ describe('GET api/v1/recipes', () => {
   });
 });
 
+describe('POST /api/v1/recipes', () => {
+  it('Should return 401 unauthorized error if no header is sent', (done) => {
+    chai.request(app)
+      .post('/api/v1/recipes')
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      })
+  })
+
+  it('Should return 403 forbidden error if a bad token is sent', (done) => {
+    chai.request(app)
+      .post('/api/v1/recipes')
+      .set("Authorization", "Bearer abc123")
+      .end((err, res) => {
+        expect(res.status).to.equal(403);
+        done();
+      })
+  })
+
+  it('Should return 409 if recipe already exists (ToDo)', (done) => {
+    // TODO
+    chai.request(app)
+      .get("/")
+      .end((err, res) => {
+        done();
+      })
+  })
+
+  it('Should return 201 if the recipe was created (ToDo)', (done) => {
+    // TODO
+    chai.request(app)
+      .get("/")
+      .end((err, res) => {
+        done();
+      })
+  })
+});
+
 describe('GET /api/v1/bad-endpoint',  () => {
   it('Should return 404', (done) => {
     chai.request(app)
@@ -27,4 +66,26 @@ describe('GET /api/v1/bad-endpoint',  () => {
         done();
       })
   });
+});
+
+describe('POST /login', () => {
+  it('Should return 401 error', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ username: "abc", password: "123" })
+      .end((err, res) => {
+        expect(res.status).to.equal(401);
+        done();
+      })
+  })
+
+  it('Should return 201 and JWT on valid credentials (ToDo)', (done) => {
+    // TODO
+    chai.request(app)
+      // .post("/login")
+      .get('/')
+      .end((err, res) => {
+        done();
+      })
+  })
 });
